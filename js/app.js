7465b963ff45fee578a088b2a914e7efe4335888
Enrique <enriquesamudio1999@gmail.com>
2021-09-26 21:58:36 -0400

    JS effect removed

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const images = document.querySelectorAll('.property__img');
-
-window.addEventListener("scroll", () => {
-    const scroll = this.scrollY / -20;
-
-    images.forEach(image => {
-        image.style.backgroundPositionY = `${scroll}px`;
-    });
-});
